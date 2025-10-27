69f45f51bf169c540697c3e778ba7fd89d47208e
F99 <farajiomar99@gmail.com>
2025-10-27 16:23:21 +0100

    Initialize Spring Boot project with JPA, MySQL, MapStruct, and Lombok configuration

---
+++ src/main/java/com/tricolv2/tricolv2/TricolV2Application.java
@@ -0,0 +1,13 @@
+package com.tricolv2.tricolv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TricolV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TricolV2Application.class, args);
+    }
+
+}
